061d03ac9492d516d51ca8d4e162fda17a416b54
woonhahaha <woonhahaha@gmail.com>
2020-03-26 16:17:43 +0900

    train and test

--- CNNs/main.py
+++ CNNs/main.py
@@ -1,24 +1,50 @@
-import os
-from models import lenet
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
+
 import torchvision
 import torchvision.transforms as transforms
 
+import os
+import argparse
+
+from dataset import *
+from models import *
+
+import time
+
+from tensorboardX import SummaryWriter
+
+import matplotlib.pyplot as plt
+import numpy as np
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
 def cifar10():
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    print('==> Preparing data...')
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ])
 
     trainset = torchvision.datasets.CIFAR10(root='../tutorial/data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                            download=True, transform=transform_train)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=16,
                                               shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='../tutorial/data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                           download=True, transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=16,
                                              shuffle=False, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat', 'deer',
@@ -26,28 +52,42 @@ def cifar10():
 
     return trainset, trainloader, testset, testloader, classes
 
-def main():
-    # print the seed value
-    seed = torch.initial_seed()
-    print('Used seed : {}'.format(seed))
+def main(args):
+    summary = SummaryWriter()
+
+    ### load model
+    mod = __import__('models', fromlist=['*'])
+    model = getattr(mod, args.model)
 
     ### Training on GPU
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    net = lenet.LeNet().to(device)
+    net = model().to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
+    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(net.parameters(), lr=args.lr)
 
     # multiply LR by 1 / 10 after every 30 epochs
     lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
     # load dataset
-    trainset, trainloader, testset, testloader, classes = cifar10()
+    data = __import__('dataset', fromlist=['*'])
+    dataset = getattr(data, args.dataset)
+    dataset = dataset()
 
-    for epoch in range(100):  # loop over the dataset multiple times
+    ### checkpoint path
+    dir_name = args.model + time.strftime('-%Y%m%d-%H%M%S', time.localtime(time.time()))
+    checkpoint_path = os.path.join('checkpoints', dir_name)
+    if not os.path.exists(checkpoint_path):
+        print('creating dir {}'.format(checkpoint_path))
+        os.mkdir(checkpoint_path)
+
+    step = 0
+    for epoch in range(2000):  # loop over the dataset multiple times
         lr_scheduler.step()
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        for i, data in enumerate(dataset.trainloader, 0):
+            step += 1
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data[0].to(device), data[1].to(device)
 
@@ -63,22 +103,61 @@ def main():
             # print statistics
             running_loss += loss.item()
             #print(i)
-            if i % 2000 == 1999:  # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+            if step % 100 == 0:  # print every 100 mini-batches
+                print("[Step: %10d] Loss: %f" % (step, loss))
+                summary.add_scalar('loss', loss, step)
+                # print('[%d, %5d] loss: %.3f' %
+                #       (epoch + 1, i + 1, running_loss / 100))
+                # summary.add_scalar('loss', loss, i)
+                # running_loss = 0.0
 
         # save checkpoints
-        checkpoint_path = os.path.join('./', 'lenet_states_e{}.pkl'.format(epoch + 1))
+        checkpoint_file_path = os.path.join(checkpoint_path, 'epoch-{}.pkl'.format(epoch))
+        print('==> Saving checkpoint... epoch {}'.format(epoch))
         state = {
             'epoch': epoch,
-            'total_steps': i,
+            'total_steps': step,
             'optimizer': optimizer.state_dict(),
             'model': net.state_dict(),
-            'seed': seed,
         }
-        torch.save(state, checkpoint_path)
-    print('Finished Training')
+        torch.save(state, checkpoint_file_path)
+    print('==> Finished Training')
+
+    ### 5. Test the network on the test data
+    dataiter = iter(dataset.testloader)
+    images, labels = dataiter.next()
+
+    # print image
+    imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' % dataset.classes[labels[j]] for j in range(4)))
+
+    outputs = net(images)
+
+    _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join('%5s' % dataset.classes[predicted[j]]
+                                  for j in range(4)))
+
+    ### performance of the network on the whole dataset
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in dataset.testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description="arguments",
+        epilog="Goodbye"
+    )
+    parser.add_argument('--model', type=str, choices=['LeNet', 'AlexNet', 'ResNet'])
+    parser.add_argument('--dataset', type=str, choices=['CIFAR10', 'ImageNet'])
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    args, _ = parser.parse_known_args()
+    main(args)
