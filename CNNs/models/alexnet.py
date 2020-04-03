a53439dacabce4a4a16c70b3293f2da2aa626fc7
woonhahaha <woonhahaha@gmail.com>
2020-04-03 15:56:32 +0900

    input size: 224x224

--- CNNs/models/alexnet.py
+++ CNNs/models/alexnet.py
@@ -2,47 +2,46 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class AlexNet(nn.Module): # for ImageNet
+class AlexNet(nn.Module): # for ImageNet (224 x 224)
     def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
             # 1st layer
             nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.LocalResponseNorm(k=2, size=5, alpha=0.0001, beta=0.75), # paper section 3.3
             # 2nd layer
             nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.LocalResponseNorm(k=2, size=5, alpha=0.0001, beta=0.75),
             # 3rd layer
             nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             # 4th layer
             nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             # 5th layer
             nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2)
         )
         self.classifier = nn.Sequential(
             # 6th layer
             nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
+            nn.Linear(256 * 5 * 5, 4096),
+            nn.ReLU(),
             # 7th layer
             nn.Dropout(p=0.5, inplace=True),
             nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             # last layer
             nn.Linear(4096, num_classes)
         )
 
     def forward(self, x):
-        print(x.shape)
         x = self.features(x)
-        print(x.shape)
+        x = x.view(-1, 256 * 5 * 5)
         x = self.classifier(x)
         return x
