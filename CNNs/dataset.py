8a1f2a7c111d82008ac0a56c9841b8b6a580f2bb
woonhahaha <woonhahaha@gmail.com>
2020-04-03 16:00:19 +0900

    add codes to resize image for AlexNet v1(original)

--- CNNs/dataset.py
+++ CNNs/dataset.py
@@ -8,11 +8,13 @@ class CIFAR10:
         print('==> Preparing data...')
         self.transform_train = transforms.Compose([
             transforms.RandomCrop(32, padding=4),
+            # transforms.Resize((224, 224)), # AlexNet v1
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         ])
         self.transform_test = transforms.Compose([
+            # transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         ])
